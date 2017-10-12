89de78ee9ca8f46654f10a18ac3d72fc1e833c47
nlau77 <nlau777@gmail.com>
2017-10-11 23:04:45 -0400

    temporary link holder slots

--- react/src/containers/Research.js
+++ react/src/containers/Research.js
@@ -41,6 +41,16 @@ class Research extends Component {
             </span>
           </div>
         </div>
+        <div className="row research-links">
+          <h5>Mechanotransduction</h5>
+          <ol>
+            <li><a href="https://doi.org/10.1038/nature09198">Grashoff, C., et al., Measuring mechanical tension across vinculin reveals regulation of focal adhesion dynamics. Nature, 2010. 466(7303): p. 263-6.</a></li>
+            <li><a href = "https://dx.doi.org/10.1083%2Fjcb.201510012">Kumar, A., et al., Talin tension sensor measurements reveal novel features of focal adhesion force transmission and mechanosensitivity. J. Cell Biol., 2016. 213(3): p. 371–383</a></li>
+            <li><a href = "https://doi.org/10.1016/j.ceb.2013.05.006">Ross, T.D., et al., Integrins in mechanotransduction. Curr. Opin. Cell Biol., 2013. 25(5): p. 613-618.</a></li>
+            <li><a href = "https://dx.doi.org/10.1101%2Fcshperspect.a005066"> M.A. Schwartz, Integrins and Extracellular Matrix in Mechanotransduction. Cold. Spring Harb. Perspect. Biol., 2010. 2(12): a005066.</a></li>
+            <li><a href = "https://doi.org/10.1038/nrm2957">Parsons J.T., et al., Cell adhesion: integrating cytoskeletal dynamics and cellular tension. Nat. Rev. Mol. Cell. Biol., 2010. 11(9): p. 633-643.</a></li>
+          </ol>
+        </div>
         <div className="membrane-photo row">
           <div className="columns small-12 small-centered">
             <h3 className="text-center membrane-label">What do we want to study?</h3>
