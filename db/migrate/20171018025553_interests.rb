a1616f1d324b658db4cb1db25714811ba8c3a782
nlau77 <nlau777@gmail.com>
2017-10-17 22:59:51 -0400

    interests tables

---
+++ db/migrate/20171018025553_interests.rb
@@ -0,0 +1,11 @@
+class Interests < ActiveRecord::Migration[5.1]
+  def change
+    create_table :interests do |t|
+      t.string :description, null: false
+      t.string :link, null: false
+      t.integer :type, null: false
+
+      t.timestamp null: false
+    end
+  end
+end
