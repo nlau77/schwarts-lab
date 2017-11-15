e4817fb4bdfe1f348a3356f59861cf5ac5ca7c4d
nlau77 <nlau777@gmail.com>
2017-11-15 16:53:20 -0500

    moved pathing of imagages

--- react/src/components/Profile.js
+++ react/src/components/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Profile = (props) => {
+  // let methodLink = "/methods/" + props.selectedMethod + "/" + props.pdf_url
+  let profilePath = "/profiles/" + props.img_url
   return (
     <div className='small-12 columns profile-section'>
-      <img className="profile-picture" src={assetHelper[`profiles/${props.img_url}`]}></img>
+      <img className="profile-picture" src={profilePath}></img>
       <div className="profile-info">
         <h3>{props.name}</h3>
         <h5>email: {props.email}</h5>
