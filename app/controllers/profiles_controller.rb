caec57ac12d047dc7d7bd3e3571fbcda0b009fa8
nlau77 <nlau777@gmail.com>
2017-11-15 17:38:42 -0500

    profile upload implemented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,39 @@
 class ProfilesController < ApplicationController
 
-def new
-@profile = Profile.new
-end
+  def new
+    @profile = Profile.new
+  end
 
-def create
+  def create
 
-end
+    # save image file
+    uploaded_io = params["profile"]["profile_image"]
+    File.open(Rails.root.join('public','profiles',
+      uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
 
-def edit
+    # grab params for profile
+    profile_params = params["profile"]
+    name = profile_params["name"]
+    email = profile_params["email"]
+    profile_image = uploaded_io.original_filename
+    description = profile_params["description"]
+    @profile = Profile.new(name:name,
+                          email:email,
+                          description: description,
+                          img_url:profile_image)
+    @profile.save
+    # binding.pry
+    redirect_to maintenance_index_path
+  end
 
-end
+  def edit
 
-def updated
+  end
 
-end
+  def updated
 
-end
+  end
+
+  end
